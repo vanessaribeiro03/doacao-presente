5af2863b3c83eedeb5f63155f7ddb7cb719ce7cb
Vanessa Ribeiro <ribeirovs2000@outlook.com>
2024-08-31 20:52:43 -0300

    atualizando o title do swagger

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Api exemplo')
+    .setTitle('Doação Presente')
     .setDescription(descriptionApi)
     .setVersion('1.0')
     .addTag('Donors')
